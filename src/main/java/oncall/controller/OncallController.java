3cafca2d88ea600803798763fbee48e1a88395ad
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-20 22:29:39 +0900

    feat: 컨트롤러 생성

--- src/main/java/oncall/controller/OncallController.java
+++ src/main/java/oncall/controller/OncallController.java
@@ -1,4 +1,40 @@
 package oncall.controller;
 
+import oncall.service.DomainService;
+import oncall.util.validater.InputValidate;
+import oncall.view.InputView;
+import oncall.view.OutputView;
+
 public class OncallController {
+    private InputView inputView;
+    private OutputView outputView;
+    private DomainService domainService;
+    public OncallController(InputView inputView, OutputView outputView, DomainService domainService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.domainService = domainService;
+    }
+
+    public void run() {
+        setDate();
+        setWeekdayWorkers();
+        setWeekendWorkers();
+    }
+
+    private void setDate() {
+        String date = inputView.inputDay();
+        InputValidate.validateDate(date);
+        domainService.setDutyWorkingDate(date);
+    }
+
+    private void setWeekdayWorkers() {
+        String weekdayEmployees = inputView.inputWeekdayWorkers();
+        InputValidate.validateWorkers(weekdayEmployees);
+        domainService.setWeekdayEmployees(weekdayEmployees);
+    }
+
+    private void setWeekendWorkers() {
+        String weekdayWorkers = inputView.inputWeekdayWorkers();
+        InputValidate.validateWorkers(weekdayWorkers);
+    }
 }
