54c8ed60d899f9fa24dca61d31ea5bd877f3c048
na0th <105775683+na0th@users.noreply.github.com>
2024-11-20 12:28:45 +0900

    feat : Month enum 추가 및 단위 테스트 추가
    추가한 내용 :
    1. Month를 검증

---
+++ src/main/java/oncall/Month.java
@@ -0,0 +1,42 @@
+package oncall;
+
+public enum Month {
+    JANUARY(1,31),
+    FEBRUARY(2,28),
+    MARCH(3,31),
+    APRIL(4,30),
+    MAY(5,31),
+    JUNE(6,30),
+    JULY(7,31),
+    AUGUST(8,31),
+    SEPTEMBER(9,30),
+    OCTOBER(10,31),
+    NOVEMBER(11,30),
+    DECEMBER(12,31);
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDays() {
+        return days;
+    }
+
+    private int month;
+    private int days;
+
+    Month(int month, int days) {
+        this.month = month;
+        this.days = days;
+    }
+
+    public static Month check(int month) {
+        for (Month value : Month.values()) {
+            if (month == value.month) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("1 ~ 12월을 입력해주세요.");
+    }
+
+}
---
+++ src/test/java/oncall/MonthTest.java
@@ -0,0 +1,30 @@
+package oncall;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MonthTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4,5,6,7,8,9,10,11,12})
+    void 제대로_된_월을_입력하면_enum을_반환한다(int month) {
+        //given
+        //when
+        Month check = Month.check(month);
+        //then
+        Assertions.assertThat(check.getMonth()).isEqualTo(month);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0,13,100})
+    void 제대로_된_월을_입력하지_않으면_예외가_발생한다(int month) {
+        //given
+        //when & then
+        Assertions.assertThatThrownBy(() -> Month.check(month))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
