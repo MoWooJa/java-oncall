45bf73b8eaa787d544957b16e1b6225cfa5c6279
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-20 22:29:27 +0900

    feat: 도메인 비즈니스 로직 사용하는 serivce 클래스 생성

--- src/main/java/oncall/service/DomainService.java
+++ src/main/java/oncall/service/DomainService.java
@@ -1,4 +1,22 @@
 package oncall.service;
 
+import oncall.domain.Calender;
+import oncall.domain.DomainFactory;
+import oncall.domain.WeekdayEmployees;
+
 public class DomainService {
+
+    private DomainFactory domainFactory;
+
+    public DomainService(DomainFactory domainFactory) {
+        this.domainFactory = domainFactory;
+    }
+
+    public void setDutyWorkingDate(String date) {
+        Calender calender = domainFactory.createCalender(date);
+    }
+
+    public void setWeekdayEmployees(String weekdayEmployee) {
+        WeekdayEmployees weekdayEmployees = domainFactory.createWeekdayEmployees(weekdayEmployee);
+    }
 }
