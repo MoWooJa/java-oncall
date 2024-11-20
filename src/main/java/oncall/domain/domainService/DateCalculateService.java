b024f903c8e4a411cd27d83e768442971cb27a4b
na0th <105775683+na0th@users.noreply.github.com>
2024-11-20 17:16:02 +0900

    feat : view 추가
    1.view 추가
    2. 공백 제거

--- src/main/java/oncall/domain/domainService/DateCalculateService.java
+++ src/main/java/oncall/domain/domainService/DateCalculateService.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class    DateCalculateService {
+public class DateCalculateService {
     public WorkerRecord calculateWeekdaysAndHolidays(ParsedInput parsedInput) {
         Month month = Month.check(parsedInput.getMonth());
 
