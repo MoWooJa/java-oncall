44501e4ccc63f872fdcdf62b1d901a34560e4716
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-20 19:08:35 +0900

    feat: 의존성 관리를 위한 AppConfig 생성

--- src/main/java/oncall/config/AppConfig.java
+++ src/main/java/oncall/config/AppConfig.java
@@ -1,4 +1,25 @@
 package oncall.config;
 
+import oncall.controller.OncallController;
+import oncall.service.DomainService;
+import oncall.view.InputView;
+import oncall.view.OutputView;
+
 public class AppConfig {
+
+    public OncallController oncallController() {
+        return new OncallController(inputView(), outputView(), domainService());
+    }
+
+    private InputView inputView() {
+        return new InputView();
+    }
+
+    private OutputView outputView() {
+        return new OutputView():
+    }
+
+    private DomainService domainService() {
+        return new DomainService();
+    }
 }
