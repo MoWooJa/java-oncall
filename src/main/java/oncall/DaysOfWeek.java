e1d3c9be058e7c4faf25475b51a065cab6f59f95
na0th <105775683+na0th@users.noreply.github.com>
2024-11-20 12:37:45 +0900

    feat : DaysOfWeek enum 및 단위 테스트 추가
    추가한 내용 :
    1. 날짜를 검증

---
+++ src/main/java/oncall/DaysOfWeek.java
@@ -0,0 +1,30 @@
+package oncall;
+
+public enum DaysOfWeek {
+    MONDAY("월"),
+    TUESDAY("화"),
+    WEDNESDAY("수"),
+    THURSDAY("목"),
+    FRIDAY("금"),
+    SATURDAY("토"),
+    SUNDAY("일");
+
+    public String getKoreanDay() {
+        return koreanDay;
+    }
+
+    private String koreanDay;
+
+    DaysOfWeek(String koreanDay) {
+        this.koreanDay = koreanDay;
+    }
+
+    public static DaysOfWeek check(String koreanDay) {
+        for (DaysOfWeek value : DaysOfWeek.values()) {
+            if (value.koreanDay.equals(koreanDay)) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("ERROR : 월화수목금토일 중 입력해주세요" + koreanDay);
+    }
+}
---
+++ src/test/java/oncall/DaysOfWeekTest.java
@@ -0,0 +1,31 @@
+package oncall;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DaysOfWeekTest {
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"월","화","수","목","금","토","일"})
+    void 정상적인_요일을_입력하면_성공한다(String koreanDay) {
+        //given
+        //when
+        DaysOfWeek daysOfWeek = DaysOfWeek.check(koreanDay);
+        //then
+        Assertions.assertThat(daysOfWeek.getKoreanDay()).isEqualTo(koreanDay);
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {"윌","황","숭","몽","긍","통","잉"})
+    void 비정상적인_요일을_입력하면_예외가_발생한다(String koreanDay) {
+        //given
+        //when & then
+        Assertions.assertThatThrownBy(()->DaysOfWeek.check(koreanDay))
+                .isInstanceOf(IllegalArgumentException.class);
+        //then
+    }
+}
