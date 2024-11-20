1ebf61da2dae5af8c219f46969ff4e8d4d82ac09
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-20 22:29:03 +0900

    feat: 평일 및 주말 사원 클래스 생성

---
+++ src/main/java/oncall/domain/WeekdayWorkers.java
@@ -0,0 +1,11 @@
+package oncall.domain;
+
+import java.util.List;
+
+public class WeekdayWorkers {
+
+    List<String> weekdayWorkersList;
+
+    public WeekdayWorkers(String WeekdayWorker) {
+    }
+}
