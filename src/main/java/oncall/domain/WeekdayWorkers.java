12563d5bd1792bf2997efd8d00a12294ee4fcbb1
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-22 10:04:16 +0900

    feat: 휴일,주말 사원 도메인 메서드 추가

--- src/main/java/oncall/domain/WeekdayWorkers.java
+++ src/main/java/oncall/domain/WeekdayWorkers.java
@@ -1,5 +1,6 @@
 package oncall.domain;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class WeekdayWorkers {
@@ -7,5 +8,26 @@ public class WeekdayWorkers {
     List<String> weekdayWorkersList;
 
     public WeekdayWorkers(String WeekdayWorker) {
+        this.weekdayWorkersList = parseWeekdayWorkers(WeekdayWorker);
+
     }
+
+    private List<String> parseWeekdayWorkers(String weekdayWorker) {
+        String[] workers = weekdayWorker.split(",");
+        List<String> checkWokrekrs = Arrays.asList(workers);
+        validate(checkWokrekrs);
+        return checkWokrekrs;
+    }
+
+    private void validate(List<String> checkWokrekrs) {
+        validateDuplicate(checkWokrekrs);
+    }
+
+    private void validateDuplicate(List<String> checkWokrekrs) {
+        if (checkWokrekrs.size() != checkWokrekrs.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 중복된 사원은 허용 되지 않음");
+        }
+    }
+
+
 }
--- src/main/java/oncall/domain/WeekendWorkers.java
+++ src/main/java/oncall/domain/WeekendWorkers.java
@@ -1,4 +1,30 @@
 package oncall.domain;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class WeekendWorkers {
+    
+    List<String> weekendWorkers;
+    
+    public WeekendWorkers(String weekendWorker) {
+        this.weekendWorkers = parseWeekendWorkers(weekendWorker);
+    }
+
+    private List<String> parseWeekendWorkers(String weekdayWorker) {
+        String[] workers = weekdayWorker.split(",");
+        List<String> checkWokrekrs = Arrays.asList(workers);
+        validate(checkWokrekrs);
+        return checkWokrekrs;
+    }
+
+    private void validate(List<String> checkWokrekrs) {
+        validateDuplicate(checkWokrekrs);
+    }
+
+    private void validateDuplicate(List<String> checkWokrekrs) {
+        if (checkWokrekrs.size() != checkWokrekrs.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 중복된 사원은 허용 되지 않음");
+        }
+    }
 }
