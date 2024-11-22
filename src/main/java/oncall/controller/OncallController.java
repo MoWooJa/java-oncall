5c706b20fbb88a4babaa36a05986d8da4addfb75
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-22 10:05:06 +0900

    feat: DutySheet도메인 생성 기능 추가

--- src/main/java/oncall/controller/OncallController.java
+++ src/main/java/oncall/controller/OncallController.java
@@ -1,5 +1,8 @@
 package oncall.controller;
 
+import oncall.domain.Calender;
+import oncall.domain.WeekdayWorkers;
+import oncall.domain.WeekendWorkers;
 import oncall.service.DomainService;
 import oncall.util.validater.InputValidate;
 import oncall.view.InputView;
@@ -16,25 +19,27 @@ public class OncallController {
     }
 
     public void run() {
-        setDate();
-        setWeekdayWorkers();
-        setWeekendWorkers();
+        Calender calender = setDate();
+        WeekdayWorkers weekdayWorkers = setWeekdayWorkers();
+        WeekendWorkers weekendWorkers = setWeekendWorkers();
+        domainService.createDutySheet(calender,weekdayWorkers,weekendWorkers);
     }
 
-    private void setDate() {
+    private Calender setDate() {
         String date = inputView.inputDay();
         InputValidate.validateDate(date);
-        domainService.setDutyWorkingDate(date);
+        return domainService.setDutyWorkingDate(date);
     }
 
-    private void setWeekdayWorkers() {
-        String weekdayEmployees = inputView.inputWeekdayWorkers();
-        InputValidate.validateWorkers(weekdayEmployees);
-        domainService.setWeekdayEmployees(weekdayEmployees);
-    }
-
-    private void setWeekendWorkers() {
+    private WeekdayWorkers setWeekdayWorkers() {
         String weekdayWorkers = inputView.inputWeekdayWorkers();
         InputValidate.validateWorkers(weekdayWorkers);
+        return domainService.setWeekdayEmployees(weekdayWorkers);
+    }
+
+    private WeekendWorkers setWeekendWorkers() {
+        String weekendWorkersStr = inputView.inputWeekendWorkers();
+        InputValidate.validateWorkers(weekendWorkersStr);
+        return  domainService.setWeekendWorkers(weekendWorkersStr);
     }
 }
--- src/main/java/oncall/domain/DomainFactory.java
+++ src/main/java/oncall/domain/DomainFactory.java
@@ -9,4 +9,12 @@ public class DomainFactory {
     public WeekdayWorkers createWeekdayWorkers(String weekdayWorkers) {
         return new WeekdayWorkers(weekdayWorkers);
     }
+
+    public WeekendWorkers createWeekendWorkers(String weekendWorker) {
+        return new WeekendWorkers(weekendWorker);
+    }
+
+    public DutySheet createDutySheet(Calender calender, WeekdayWorkers weekdayWorkers, WeekendWorkers weekendWorkers) {
+        return new DutySheet(calender, weekdayWorkers, weekendWorkers);
+    }
 }
